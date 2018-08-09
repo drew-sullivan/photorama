f2c0db780db807b15c981689e1edab5b95f5a38d
Drew Sullivan <dsullivan@dev-030416.local>
2018-08-09 09:40:51 -0400

    First real commit

---
+++ PhotosViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Drew Sullivan on 8/9/18.
+//  Copyright © 2018 Drew Sullivan, DMA. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController : UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+}
