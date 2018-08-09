b96663c613df80fff3952a5262675fcd8b0c7216
Drew Sullivan <dsullivan@dev-030416.local>
2018-08-09 12:22:24 -0400

    Adding web services and getting ready to download and display image data

---
+++ PhotoStore.swift
@@ -0,0 +1,40 @@
+//
+//  PhotoStore.swift
+//  Photorama
+//
+//  Created by Drew Sullivan on 8/9/18.
+//  Copyright © 2018 Drew Sullivan, DMA. All rights reserved.
+//
+
+import Foundation
+
+enum PhotosResult {
+    case success([Photo])
+    case failure(Error)
+}
+
+class PhotoStore {
+    
+    private let session: URLSession = {
+        let config = URLSessionConfiguration.default
+        return URLSession(configuration: config)
+    }()
+    
+    func fetchInterestingPhotos(completion: @escaping (PhotosResult) -> Void) {
+        let url = FlickrAPI.interestingPhotosURL
+        let request = URLRequest(url: url)
+        let task = session.dataTask(with: request) {
+            (data, response, error) -> Void in
+            let result = self.processPhotosRequest(data: data, error: error)
+            completion(result)
+        }
+        task.resume()
+    }
+    
+    private func processPhotosRequest(data: Data?, error: Error?) -> PhotosResult {
+        guard let jsonData = data else {
+            return .failure(error!)
+        }
+        return FlickrAPI.photos(fromJSON: jsonData)
+    }
+}
