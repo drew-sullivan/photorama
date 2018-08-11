50b9b40e9f2b1d3557145ed19de36559b54cc4c2
Drew Sullivan <drew.sullivan.dma@gmail.com>
2018-08-11 16:29:05 -0400

    Fixed ibaction bug misconnection

--- Photorama/TagsViewController.swift
+++ Photorama/TagsViewController.swift
@@ -18,6 +18,39 @@ class TagsViewController: UITableViewController {
     
     let tagDataSource = TageDataSource()
     
+    @IBAction func done(_ sender: UIBarButtonItem) {
+        presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func addNewTag(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "Add Tag", message: nil, preferredStyle: .alert)
+        
+        alertController.addTextField {
+            (textField) -> Void in
+            textField.placeholder = "tag name"
+            textField.autocapitalizationType = .words
+        }
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) {
+            (action) -> Void in
+            if let tagName = alertController.textFields?.first?.text {
+                let context = self.store.persistentContainer.viewContext
+                let newTag = NSEntityDescription.insertNewObject(forEntityName: "Tag", into: context)
+                newTag.setValue(tagName, forKey: "name")
+                do {
+                    try self.store.persistentContainer.viewContext.save()
+                } catch let error {
+                    print("Core data save failed \(error)")
+                }
+                self.updateTags()
+            }
+        }
+        alertController.addAction(okAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -48,6 +81,34 @@ class TagsViewController: UITableViewController {
                 print("Error fetching tags: \(error)")
             }
             self.tableView.reloadSections(IndexSet(integer: 0), with: .automatic)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let tag = tagDataSource.tags[indexPath.row]
+        
+        if let index = selectedIndexPaths.index(of: indexPath) {
+            selectedIndexPaths.remove(at: index)
+            photo.removeFromTags(tag)
+        } else {
+            selectedIndexPaths.append(indexPath)
+            photo.addToTags(tag)
+        }
+        
+        do {
+            try store.persistentContainer.viewContext.save()
+        } catch {
+            print("Core Data save failed: \(error)")
+        }
+        
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if selectedIndexPaths.index(of: indexPath) != nil {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
         }
     }
 }
